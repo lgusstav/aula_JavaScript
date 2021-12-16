118d4481ed34771185762a96d48660aa4c0258d4
Gustav <luizgustavocaneirolima1312@gmail.com>
2021-12-16 16:51:35 -0300

    primeiras criações de JS

---
+++ js/main.js
@@ -0,0 +1,10 @@
+var nome = "Luiz Gustavo";
+var n = 5;
+var n2 = 3;
+var fs = "Flamengo é o melhor time do mundo"
+
+//alert(nome + " tem " + id + " anos");
+
+console.log(nome);
+console.log(n * n2);
+console.log(fs.toLowerCase() );
