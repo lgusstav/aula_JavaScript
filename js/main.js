db8f90c4bba11a5b3c444f13326938007df843d1
Gustav <luizgustavocaneirolima1312@gmail.com>
2021-12-20 15:36:21 -0300

    "alterações da terceira aula"

--- js/main.js
+++ js/main.js
@@ -1,9 +1,31 @@
+var D = new Date ();
+alert(D.getMinutes());
+alert(D.getDay());
+alert(D.getHours());
+/*
+var count = 0;
+for( count=0; count <=5; count++){
+alert(count);
+};
+
+var count = 0;
+while (count <= 5) { 
+ console.log (count);
+ alert (count);
+ count = count + 1;
+};
+
+var id= prompt ("Qual é a sua idade?");
+if (id >= 18){
+ alert("maior de idade");
+}else{
+    alert("menor de idade");
+   };
+
 var fts = [{name:"maça", cor:"vermelha"}, {name:"pera", cor:"verde"}]
 console.log(fts);
 alert(fts[1].name);
 
-
-/*
 var ft = {name:"maça", cor:"vermelha"};
 console.log(ft.cor);
 alert(ft.name);
